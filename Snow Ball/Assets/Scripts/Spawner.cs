cb7cb801df3368afdd7a54695c28c9b2dc2133d6
NaylaVRbh <71579324+NaylaVRbh@users.noreply.github.com>
2021-05-06 22:41:55 -0400

    prefab asn

---
+++ Snow Ball/Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject snowballPrefab;
+    [SerializeField] private GameObject SpawnPoint;
+    [SerializeField] private GameObject MainCamera;
+    private GameObject CurrentSnowBall;
+    private bool isTraveling = false;
+
+    public void SpawnSnowball()
+    {
+        StartCoroutine("SpawnSnow");
+    }
+    
+
+    void Update()
+    {
+        if(isTraveling)
+        {
+            CurrentSnowBall.transform.position -= new Vector3(0, 0, 30 * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnSnowball();
+        }
+
+    }
+
+    IEnumerator SpawnSnow()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        isTraveling = true;
+
+        GameObject snowball = Instantiate(snowballPrefab, SpawnPoint.transform.position, transform.rotation);
+
+        CurrentSnowBall = snowball;
+
+        snowball.transform.LookAt(MainCamera.transform);
+    }
+
+}
