57ab2d79493a8fce1a7d7281b352349847834f95
NaylaVRbh <71579324+NaylaVRbh@users.noreply.github.com>
2021-05-06 22:29:17 -0400

    ring

---
+++ Moving Ring Excersice(1)/Assets/Scripts/Ring.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    [SerializeField] private float RotationSpeed;
+    
+    void Update()
+    {
+        RotateRing();
+    }
+
+    private void RotateRing()
+    {
+        transform.Rotate(Vector3.right * RotationSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Moving Ring Excersice(1)/Assets/Scripts/Spawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject Cube;
+
+    private bool isSpawnRunning = false;
+
+    private GameObject ins;
+
+    [SerializeField] private float CubeSpeed;
+    
+
+    private void Start()
+    {
+        isSpawnRunning = false;
+    }
+
+    private void Update()
+    {
+        if(isSpawnRunning)
+        {
+            RunSpawn(ins);
+            //StartCoroutine("DestroySpawn");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject currentSpawn = SpawnCube();
+            isSpawnRunning = true;
+        }
+        
+    }
+
+    private GameObject SpawnCube()
+    {
+        ins = Instantiate(Cube, transform.position, Quaternion.identity);
+
+        return ins;
+    }
+
+    private void RunSpawn(GameObject CurrentSpawn)
+    {
+        CurrentSpawn.transform.position += new Vector3(CubeSpeed * Time.deltaTime, 0, 0);
+    }
+
+    //IEnumerator DestroySpawn()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //    Destroy(ins);
+    //}
+}
